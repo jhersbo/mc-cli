9daf07f8ddbe584a7b6f842f04d9d151ae991490
jhersbo <jhersbo@gmail.com>
2025-09-18 09:57:34 -0500

    release v0.0.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='mc-cli',
-    version='0.3',
+    version='0.0.3',
     packages=find_packages(),
     install_requires=[
         'click',
