8a1699129310dd16e630e41fa1b158f6128b0401
jhersbo <jhersbo@gmail.com>
2025-09-18 10:06:48 -0500

    fix setup

--- setup.py
+++ setup.py
@@ -3,6 +3,8 @@ from setuptools import setup, find_packages
 setup(
     name='mc-cli',
     version='0.0.3',
+    author='jhersbo',
+    description='A simple CLI to manage a Minecraft Bedrock Server running in Docker.',
     packages=find_packages(),
     install_requires=[
         'click',
